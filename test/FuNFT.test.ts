fe28b94eb02b1595c93de8631b10a0df3b8d7ac1
Jim Neuendorf <jim.neuendorf@gmx.de>
2023-02-10 23:14:03 +0100

    more NFT tests

--- test/FuNFT.test.ts
+++ test/FuNFT.test.ts
@@ -7,7 +7,11 @@ import FuNFT from '../build/FuNFT.json';
 use(solidity);
 
 describe('FuNFT', () => {
-    const [wallet, walletTo] = new MockProvider().getWallets();
+    const [
+        wallet,
+        walletTo,
+        walletTo2,
+    ] = new MockProvider().getWallets();
     let token: Contract;
 
     beforeEach(async () => {
@@ -20,13 +24,13 @@ describe('FuNFT', () => {
         expect(await token.balanceOf(wallet.address)).to.equal(0);
     });
 
-    it.only('Can be minted', async () => {
+    it('Can be minted', async () => {
         const level = 1;
         const isUpgrade = false;
         await token.mint(level, isUpgrade);
     });
 
-    it.only('Correctly outputs total supply and owned tokens after minting', async () => {
+    it('Correctly outputs total supply and owned tokens after minting', async () => {
         const level = 1;
         const isUpgrade = false;
         await token.mint(level, isUpgrade);
@@ -35,13 +39,55 @@ describe('FuNFT', () => {
         expect(await token.ownedTokens(walletTo.address)).to.have.length(1);
     });
 
-    it.only('Correctly outputs token info', async () => {
-        const level = 1;
-        const isUpgrade = false;
-        await token.mint(level, isUpgrade);
-        // TODO: the returned TokenInfo cannot be accessed unfortunenately
-        expect(await token.getLevel(0)).to.equal(level);
-        expect(await token.isUpgrade(0)).to.equal(isUpgrade);
+    describe('Minting', () => {
+        it('Can be minted with level == 1', async () => {
+            const level = 1;
+            const isUpgrade = false;
+            await token.mint(level, isUpgrade);
+            // TODO: the returned TokenInfo cannot be accessed unfortunenately
+            expect(await token.getLevel(0)).to.equal(level);
+            expect(await token.isUpgrade(0)).to.equal(isUpgrade);
+        });
+
+        it('Can be minted with level > 1', async () => {
+            const level = 3;
+            const isUpgrade = false;
+            await token.mint(level, isUpgrade);
+            expect(await token.getLevel(0)).to.equal(level);
+            expect(await token.isUpgrade(0)).to.equal(isUpgrade);
+        });
+
+        it('Can be minted with level > 1 && isUpgrade', async () => {
+            const level = 3;
+            const isUpgrade = true;
+            await token.mint(level, isUpgrade);
+            expect(await token.getLevel(0)).to.equal(level);
+            expect(await token.isUpgrade(0)).to.equal(isUpgrade);
+        });
+
+        it('Can be minted multiple times', async () => {
+            const level = 3;
+            const isUpgrade = true;
+            await token.mint(level, isUpgrade);
+            await token.mint(level, isUpgrade);
+            expect(await token.totalSupply()).to.equal(2);
+        });
+
+        it.only('Can be minted multiple times to different recipients', async () => {
+            await token.mintTo(2, false, walletTo.address);
+            await token.mintTo(1, true, walletTo2.address);
+            await token.mintTo(2, true, walletTo2.address);
+
+            expect(await token.totalSupply()).to.equal(3);
+
+            expect(await token.balanceOf(wallet.address)).to.equal(0);
+            expect(await token.balanceOf(walletTo.address)).to.equal(1);
+            expect(await token.balanceOf(walletTo2.address)).to.equal(2);
+
+            expect(await token.ownedTokens(wallet.address)).to.have.length(0);
+            expect(await token.ownedTokens(walletTo.address)).to.have.length(1);
+            expect(await token.ownedTokens(walletTo2.address)).to.have.length(2);
+        });
     });
 
     // TODO:
