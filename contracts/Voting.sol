09bb37a843dfca7efd8e2d4589dcf99daeb0f20e
rynzaro <rynzaro44@gmail.com>
2023-02-12 11:00:37 +0100

    Change requires

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -79,16 +79,21 @@ contract Voting {
     }
 
     function endPoll(uint256 _pollID) external onlyOwner {
+        require(getStatus(_pollID) == Status.IN_PROGRESS || _pollID < _pollIdCounter.current() , "Poll is not in progress or pollID does not exist");
         Poll storage poll = polls[_pollID];
         poll.status = Status.CLOSED;
         returnCoinsAfterPoll(_pollID);
     }
 
     function castVote(uint256 _pollID, uint256 _optionIndex, uint256 weight) public {
-        require(getStatus(_pollID) == Status.IN_PROGRESS, "Poll is not in progress!");
+        require(getStatus(_pollID) == Status.IN_PROGRESS || _pollID < _pollIdCounter.current() , "Poll is not in progress or pollID does not exist");
         // check if msg.sender has the required balance to vote their desired weight
         require(_voteToken.balanceOf(msg.sender) >= weight, "Not enough vote tokens");
         require(weight > 0, "Cannot vote with 0 tokens!");
+        // check if the poll allows multiple options and then if the voter has already voted
+        require(polls[_pollID].allowMultipleOptions || polls[_pollID].addressToTotalVotes[msg.sender] == 0, "You can only vote once when allowMultipleOptions is set to false");
+        require(polls[_pollID].votingOptionsCount > _optionIndex, "The optionIndex is higher than the amount of options");
+
         address voter = msg.sender;
         // requires approval from the voter beforehand to transfer coins from his/her account to this contract owner address
         // to do that call the approve-method in the ERC20-Token before casting the vote
